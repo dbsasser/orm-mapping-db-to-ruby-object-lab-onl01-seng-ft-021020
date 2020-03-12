add89c8cae142371dc914ee526157af84acf8d76
Daniel Sasser <danielbsasser@gmail.com>
2020-03-12 04:13:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,7 +83,9 @@ class Student
       SELECT * FROM students WHERE grade = 0 LIMIT #{num_of_students}
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_student_in_grade_10
