62a3e50e3defd512522fce47acc66f383e837900
Daniel Sasser <danielbsasser@gmail.com>
2020-03-12 04:12:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,9 @@ class Student
       SELECT * FROM students WHERE grade < 12
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_X_students_in_grade_10(num_of_students)
