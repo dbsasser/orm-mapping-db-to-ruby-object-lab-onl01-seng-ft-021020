211289d7e6e5ecf4d3b3686ebbbb4f65c8849222
Daniel Sasser <danielbsasser@gmail.com>
2020-03-12 04:22:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,7 @@ class Student
 
   def self.first_X_students_in_grade_10(num_of_students)
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 0 LIMIT #{num_of_students}
+      SELECT * FROM students WHERE grade = 10 LIMIT #{num_of_students}
     SQL
 
     DB[:conn].execute(sql).map do |row|
